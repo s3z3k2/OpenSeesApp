24c9fbcde9e8048043c4e856637f24594b62c5d4
s3z3k2 <suzuki.nobuya.95@gmail.com>
2017-10-30 23:55:37 +0900

    モデルクリック時に色が変化する

--- js/generate.js
+++ js/generate.js
@@ -2,33 +2,36 @@ var vm = new Vue({
   el: "#app", // Vue.jsを使うタグのIDを指定
   data: {
     // Vue.jsで使う変数はここに記述する
-
+    isActive_b1 : false,isActive_b2 : false,isActive_b3 : false,
+    isActive_b4 : false,isActive_b5 : false,isActive_b6 : false,
+    isActive_b7 : false,isActive_b8 : false,isActive_b9 : false,
+    isActive_b10 : false,isActive_b11 : false,isActive_b12 : false,
+    isActive_b13 : false,isActive_b14 : false,isActive_b15 : false,
+    isActive_b16 : false,isActive_b17 : false,isActive_b18 : false,
+    isActive_b19 : false,isActive_b20 : false,isActive_b21 : false,
+    isActive_b22 : false,isActive_b23 : false,isActive_b24 : false,
+    isActive_b25 : false,isActive_b26 : false,isActive_b27 : false,
+    isActive_b28 : false,isActive_b29 : false,isActive_b30 : false,
+    isActive_c1 : false,isActive_c2 : false,isActive_c3 : false,isActive_c4 : false,
+    isActive_c5 : false,isActive_c6 : false,isActive_c7 : false,isActive_c8 : false,
+    isActive_c9 : false,isActive_c10 : false,isActive_c11 : false,isActive_c12 : false,
+    isActive_c13 : false,isActive_c14 : false,isActive_c15 : false,isActive_c16 : false,
+    isActive_c17 : false,isActive_c18 : false,isActive_c19 : false,isActive_c20 : false,
+    isActive_c21 : false,isActive_c22 : false,isActive_c23 : false,isActive_c24 : false,
+    isActive_c25 : false,isActive_c26 : false,isActive_c27 : false,isActive_c28 : false,
+    isActive_c29 : false,isActive_c30 : false,isActive_c31 : false,isActive_c32 : false,
+    isActive_c33 : false,isActive_c34 : false,isActive_c35 : false,isActive_c36 : false,
+    isActive_c37 : false,isActive_c38 : false,isActive_c39 : false,isActive_c40 : false,
   },
   methods: {
     // Vue.jsで使う関数はここで記述する
-    click: function() {
-
-    },
-
-    GetCell: function(Cell){
-      var rowINX = '行位置：'+Cell.parentNode.rowIndex;//Cellの親ノード'tr'の行位置
-      var cellINX = 'セル位置：'+Cell.cellIndex;
-      alert(rowINX)
-      alert(cellINX)
-    },
-
-    over: function() {
-      var myTbl = document.getElementById('TBL');
-      var Td = myTbl.getElementsByTagName('td'); // タグ 'td'を取得
-      for(var i=0 ;i<Td.length; i++){ //tdの数だけループ
-        Td[i].onclick=function(){vm.GetCell(this);}
-      }
-    }
   },
   created: function() {
     // Vue.jsの読み込みが完了したときに実行する処理はここに記述する
   },
   computed: {
     // 計算した結果を変数として利用したいときはここに記述する
+
+
   }
 });
